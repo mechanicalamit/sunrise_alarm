ef9353055c3fdb5cbcc50792322de48c629ac18f
Amit Dhingra <mechanicalamit@gmail.com>
2018-08-29 22:14:44 -0400

    Added logging capability
    Basic functionality. Log file has starting date/time in filename
    
    Log file adds alarm_db and then all transitions to state machine(s)

--- sa_lib/display_prettytable.py
+++ sa_lib/display_prettytable.py
@@ -22,6 +22,15 @@ class display(object):
         al_db = AlarmsDB(settings.al_db_file)
         self.aldbrawtext = al_db.get_rawtext()
 
+        if settings.logging:
+            self.logfile_name = 'sa_log_'+ datetime.now(tz=self.ET_TZ).strftime("%Y%m%d_%H%M") + '.log'
+            with open(self.logfile_name, 'w') as lf:
+                lf.write('No.\tEnabled\tDOW\tReady_T\tRun_T\tRepeat\tCounter\tProfile\n')
+                for line in self.aldbrawtext:
+                    lf.write(line+'\n')
+                lf.write('\n' * 2)
+                lf.write('Al.No\tWall Time\t\tStamp Time\t\tState\n')
+
     def add_t(self, al, state, timestamp):
         #et_time = datetime.now(tz=self.ET_TZ).strftime("%Y/%m/%d %H:%M:%S")
         if al is None:
@@ -36,6 +45,10 @@ class display(object):
 
         if self.t_no > 15 :
             self.x.del_row(0)
+
+        if settings.logging:
+            with open(self.logfile_name, 'a') as lf:
+                lf.write(str(al_no) +'\t'+ et_time.strftime(t_fmt) +'\t'+ timestamp.strftime(t_fmt) +'\t'+ state +'\n')
 
     def print(self, rgbvals):
         print('\r                                              ', end="")
